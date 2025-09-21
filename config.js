df8836545584c76e4217e4fa99c1169c1171c715
HARSH-YADUVANSHI02 <harshrao022006@gmail.com>
2025-09-21 21:38:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,10 @@
-
+// --- Firebase Configuration ---
+export const firebaseConfig = {
+    apiKey: "AIzaSyDQWhTpjDnzRHUrhKcgVq2Ueih5IGiQkW4",
+    authDomain: "iilm-attendance-tracke.firebaseapp.com",
+    projectId: "iilm-attendance-tracke",
+    storageBucket: "iilm-attendance-tracke.appspot.com",
+    messagingSenderId: "1076382702822",
+    appId: "1:1076382702822:web:26fbd843f7768cd2adef27",
+    measurementId: "G-79L33P2FPJ"
+};
